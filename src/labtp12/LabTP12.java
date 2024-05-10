d47439fcf1117a5f2d500ebb50d722ab575bd76f
Virginia <virfederici@gmail.com>
2024-05-10 17:00:46 -0300

    Se listan herramientas y se da de baja el primer empleado.

--- src/labtp12/LabTP12.java
+++ src/labtp12/LabTP12.java
@@ -23,26 +23,49 @@ public class LabTP12 {
         try {
 
             Class.forName("org.mariadb.jdbc.Driver");
-            String bd = "jdbc:mysql://localhost/construirsa";
+            String bd = "jdbc:mysql://localhost:3307/construirsa";
             String usuario = "root";
             String password = "";
             Connection con = DriverManager.getConnection(bd, usuario, password);
-            String sql = "INSERT INTO "
-                    + "herramientas(nombre, descripcion, stock, estado)"
-                    + "VALUES ('martillo','para dar los mejores golpes',3,1),"
-                    + "('llave boca 13-14','13-14',3,1),"
-                    + "('serrucho','para serruchar el piso',6,1)";
 
+//  Estas sentencias son ejecutadas para modificar la bd
+//            String sql = "INSERT INTO "
+//                    + "herramientas(nombre, descripcion, stock, estado)"
+//                    + "VALUES ('martillo','para dar los mejores golpes',3,1),"
+//                    + "('llave boca 13-14','13-14',3,1),"
+//                    + "('serrucho','para serruchar el piso',6,1)";
 //            String sql = "INSERT INTO "
 //                    + "empleado( dni, apellido, nombre, acceso, estado)"
 //                    + "VALUES (23567823,'Lopez','Juan',1,1),"
 //                    + "(23774323,'Suarez','Mario',1,1),"
 //                    + "(23534323,'Sosa','Miguel',1,1)";
-            PreparedStatement ps = con.prepareStatement(sql);
+//            PreparedStatement ps = con.prepareStatement(sql);           
+//            int filas = ps.executeUpdate();
+//            if (filas > 0) {
+//                JOptionPane.showMessageDialog(null, "Datos actualizados");
+//            }
+
+//  Estas sentencias son ejecutadas para traer información de la bd
+            // creo un objeto de tipo statement para realizar una sentencia sql
+            Statement mi_statement = con.createStatement();
+            // creo un objeto de tipo resultset para guardar los resultados que devuelve la bd
+            ResultSet mi_resulset = mi_statement.executeQuery("SELECT * FROM herramientas WHERE stock>10;"); // Resulset es una tabla virtual
+
+            //5. Recorrer/leer el resulset
+//            while (mi_resulset.next()) {
+//                System.out.println(mi_resulset.getString("id_herramienta") + " " + mi_resulset.getString("nombre"));
+//
+//            }
+// Esta sentencia se utiliza para actualizar registros
+String sql = "UPDATE `empleado` "
+        + " SET `id_empleado`=1,`estado`= 0"
+        + " WHERE id_empleado=1";
+PreparedStatement ps = con.prepareStatement(sql); 
             int filas = ps.executeUpdate();
             if (filas > 0) {
                 JOptionPane.showMessageDialog(null, "Datos actualizados");
             }
+            
         } catch (ClassNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "Error carga Driver");
         } catch (SQLException ex) {
